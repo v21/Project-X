cf4588cb739df1ccbc1830d06c48df8ed3d18454
George Buckenham <v21@tardis.ed.ac.uk>
2009-12-14 20:18:09 +0000

    fetch_new() (in feeds) now updates points as well as returning the new items

--- feeds/models.py
+++ feeds/models.py
@@ -10,21 +10,24 @@ class FeedBuilding(Building):
     score_per_item = models.IntegerField(default=10) #do we need a more complex representaton?
     
     def __unicode__(self):
-        return self.text
+        return self.name
 
     def fetch_new(self):
 
         feed = feedparser.parse(self.feedurl)
-        new_feeds = []
+        new_items= []
         for e in feed.entries:
             if datetime(*e.published_parsed[:6]) > self.last_checked:
-                new_feeds += [e]
+                new_items += [e]
             else:
                 pass #break #cuz theyre chronological, we can do this. yes?
-
-        last_checked = datetime.now()
-        return new_feeds
         
+        new_points = score_per_item * len(new_items) + self.current_score().points
+        score = Score(points = new_points, parent = self)
+        score.save()
+        last_checked = datetime.now()
+        return new_items
+
     def __init__(self, *args, **kwargs):
         super(FeedBuilding, self).__init__(*args, **kwargs)
         self.fetch_new()
